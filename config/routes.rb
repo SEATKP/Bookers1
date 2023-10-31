86db06418daeb64037e2b32833e0fae942939a32
EC2 Default User <ec2-user@ip-172-31-29-216.ap-northeast-1.compute.internal>
2023-10-31 05:19:31 +0000

    課題提出

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+ root :to => 'homes#top'
+ # get 'books/new'
+ # post 'books' => 'books#create'
+ # get 'books' => 'books#index'
+ # patch 'books/:id' => 'books#update', as: 'update_book'
+ # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
 end
